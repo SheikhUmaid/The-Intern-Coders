e7c47224f293286442c6e16969e9163001951d0b
sheikh umaid <73013686+SheikhUmaid@users.noreply.github.com>
2022-01-19 14:59:22 +0530

    Update urls.py

--- TheInternCoders/urls.py
+++ TheInternCoders/urls.py
@@ -16,12 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
-from django.views.static import serve
-from django.conf.urls import url
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.Index,name='HOME'),
     path('getadmission',views.Admission,name='HOME'),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
