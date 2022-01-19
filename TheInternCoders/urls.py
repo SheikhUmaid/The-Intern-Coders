01b0b869f40716b91feaacd5e37cc74ead5f9646
sheikh umaid <73013686+SheikhUmaid@users.noreply.github.com>
2022-01-19 14:09:51 +0530

    Update urls.py

--- TheInternCoders/urls.py
+++ TheInternCoders/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
-
+from django.views.static import serve
+from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.Index,name='HOME'),
